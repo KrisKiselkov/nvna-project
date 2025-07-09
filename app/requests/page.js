3b5b9e4c1e8dd93d0f5ddbb235bdb4d334192b65
Kristian Kiselkov <Kristian>
2025-07-10 01:31:28 +0300

    Requests Full Functionality

--- app/requests/page.js
+++ app/requests/page.js
@@ -3,10 +3,55 @@
 import styles from './requests.module.css';
 import { requestData } from './requestData';
 import { useState } from 'react';
+import Link from 'next/link';
 
 
 export default function RequestsPage() {
+
+    // State to hold the search query
+    const [searchQuery, setSearchQuery] = useState("");
+    const handleSearchChange = (event) => {
+        setSearchQuery(event.target.value);
+    }
+
+    const [ searchFilter, setSearchFilter ] = useState("Номер");
+    const handleSearchFilterChange = (event) => {
+        setSearchFilter(event.target.value);
+    }
+
+
+    // State to hold the filter value
+    const [ filter, setFilter ] = useState("Всички");
+    const handleFilterChange = (event) => {
+        setFilter(event.target.value);
+    };
+
+    
+    // State to hold the request data
     const [ requestDataState, setRequestDataState ] = useState(requestData);
+    
+    const filteredRequests = requestDataState.filter(request => {
+        if (filter === "Отпуски" || filter === "Командировки") {
+            return request.type === filter
+        } else {
+            return true;
+        }
+    });
+
+
+    const searchRequests = filteredRequests.filter(request => {
+        const query = searchQuery.toLowerCase();
+        if (searchFilter === "Номер") {
+            return request.id.toString().includes(query);
+        } else if (searchFilter === "Email") {
+            return request.email.toLowerCase().includes(query);
+        } else if (searchFilter === "Тел. Номер") {
+            return request.phone.includes(query);
+        } else if (searchFilter === "Дата") {
+            return request.date.includes(query);
+        }
+    });
+    
     
     // Pagination logic
     const itemsPerPage = 10;
@@ -14,9 +59,9 @@ export default function RequestsPage() {
 
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = startIndex + itemsPerPage;
-    const currentItems = requestDataState.slice(startIndex, endIndex);
+    const currentItems = searchRequests.slice(startIndex, endIndex);
 
-    const totalPages = Math.ceil(requestDataState.length / itemsPerPage);
+    const totalPages = Math.ceil(filteredRequests.length / itemsPerPage);
 
     const nextPage = () => {
         if (page < totalPages) setPage(page + 1);
@@ -40,17 +85,41 @@ export default function RequestsPage() {
         }
     }
 
+    // State to hold the selected request and requested link display
+    const [ selectedRequest, setSelectedRequest ] = useState(null);
+    const [ requestedLinkDisplay, setRequestedLinkDisplay ] = useState(false);
+
+    // Function to display the requested link
+    const requestedLink = (request) => {
+        console.log(selectedRequest);
+        return (
+            <div className={styles["main__requst-link-overlay"] + (requestedLinkDisplay ? " " + styles["requst-link-overlay--active"] : "")}>
+            <div className={styles["requst-link-overlay__requset-link-div"]}>
+                <p className={styles["requset-link-div__x"]} onClick={() => setRequestedLinkDisplay(false)}>X</p>
+                <a className={styles["requset-link-div__a"]}>{request.email}</a>
+
+                <div className={styles["requset-link-div__approve-reject-btns"]}>
+                    <button className={`${styles["approve-reject-btns__btn"]} ${styles["approve-reject-btns__btn--approve"]}`}>Потвърди</button>
+                    <button className={`${styles["approve-reject-btns__btn"]} ${styles["approve-reject-btns__btn--reject"]}`}>Отхвърли</button>
+                </div>
+            </div>
+        </div>
+        )
+    }
+
     // Map through the requestDataState to create table rows
     const allRequests = currentItems.map((request, index) => (
-        <tr key={request.id}>
-            <td>{index + 1}</td>
+        <tr onClick={() => {
+            setSelectedRequest(request)
+            setRequestedLinkDisplay(true);
+        }} key={request.id}>
+            <td>{request.id}</td>
             <td>{request.email}</td>
             <td>{request.phone}</td>
             <td>{request.date}</td>
             <td><p className={`${styles['tbody__status']} ${styles[getStatusClass(request.status)]}`}>{request.status}</p></td>
         </tr>
     ));
-
 
 
     return (
@@ -73,10 +142,11 @@ export default function RequestsPage() {
                 <div className={styles["main-card__main-card-body"]}>
                     <div className={styles["main-card-body__body-sel-doc"]}>
                     <p className={styles["body-sel-doc__p"]}>Избери вид документ</p>
-                    <select className={styles["body-sel-doc__sel-doc"]}>
-                        <option className={styles["sel-doc__option"]}>Отпуски</option>
-                        <option className={styles["sel-doc__option"]}>Друго</option>
-                        <option className={styles["sel-doc__option"]}>Друго</option>
+                    <select className={styles["body-sel-doc__sel-doc"]} 
+                    onChange={handleFilterChange}>
+                        <option className={styles["sel-doc__option"]}>Всички</option>
+                        <option className={styles["sel-doc__option"]} value="Отпуски">Отпуски</option>
+                        <option className={styles["sel-doc__option"]} value="Командировки">Командировки</option>
                     </select>
                     </div>
 
@@ -89,15 +159,18 @@ export default function RequestsPage() {
                         <input
                         type="search"
                         className={styles["bad-search__input"]}
-                        placeholder="Търси по име"
+                        placeholder="Търси..."
+                        onChange={handleSearchChange}
+                        value={searchQuery}
                         />
                         <div className={styles["bad-search__search-by"]}>
                         <p className={styles["search-by__p"]}>Търси по</p>
-                        <select className={styles["search-by__sb-select"]}>
-                            <option className={styles["sb-select__option"]}>Номер</option>
-                            <option>Email</option>
-                            <option>Тел. Номер</option>
-                            <option>Дата</option>
+                        <select className={styles["search-by__sb-select"]}
+                        onChange={handleSearchFilterChange}>
+                            <option className={styles["sb-select__option"]} value="Номер">Номер</option>
+                            <option value="Email">Email</option>
+                            <option value="Тел. Номер">Тел. Номер</option>
+                            <option value="Дата">Дата</option>
                         </select>
                         </div>
                     </div>
@@ -122,7 +195,7 @@ export default function RequestsPage() {
                                 <tr>
                                     <td colSpan={5}>
                                         <div className={styles["tfoot__flex"]}>
-                                            <p className={styles["tfoot__p"]}>Показване на {Math.min(endIndex, requestData.length)} от {requestData.length} заявки</p>
+                                            <p className={styles["tfoot__p"]}>Показване на {Math.min(endIndex, searchRequests.length)} от {searchRequests.length} заявки</p>
                                             <div className={styles["tfoot__table-page-nav"]}>
                                                 <p className={styles["table-page-nav__p"]}>Страница {page} от {totalPages}</p>
                                                 <button onClick={prevPage} className={`${styles["table-page-nav__tpn-btn"]} ${styles["tpn-btn--back"]}`}>
@@ -141,6 +214,8 @@ export default function RequestsPage() {
                 </div>
             </div>
         </div>
+
+        {requestedLinkDisplay && selectedRequest && requestedLink(selectedRequest)}
     </div>
   );
 }
