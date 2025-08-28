6b0283670d19474a8a96de09079e5aa1b07874b6
Kristian Kiselkov <Kristian>
2025-08-28 23:57:13 +0300

    User Dashboard Base Look

--- app/client/requestsClient/page.js
+++ app/client/requestsClient/page.js
@@ -1,11 +1,35 @@
+"use client";
+
 import styles from './requestsClient.module.css';
+import { useState } from 'react';
 
 
 export default function RequestsClient () {
+    const tasks = [1, 2, 3]; // example tasks
+    const [taskDone, setTaskDone] = useState(Array(tasks.length).fill(false));
+
+    const toggleTask = (index) => {
+        setTaskDone((prev) =>
+            prev.map((s, i) => (i === index ? !s : s))
+        );
+    };
+
+    const allTasks = () => {
+        return tasks.map((task, index) => (
+            <div className={styles["user-db-divs__tasks-div"]} key={index}>
+                <div key={index} className={styles["tasks-div__task-end-date-div"] + (taskDone[index] ? " " + styles.taskDone : "")}>
+                    <p className={styles["tasks-div__task-p"]}>Задача {task}</p>
+                    <p className={styles["tasks-div__end-date-p"]}>17.08.2025</p>
+                </div>
+                <button onClick={() => toggleTask(index)} className={styles["tasks-div__btn"]}>&#10004;</button>
+            </div>
+        ));
+    }
+
     return (
         <div className={styles["main-requests-client"]}>
             <div className={styles["main-requests-client__mrc-header"]}>
-                <h3 className={styles["mrc-header__h3"]}>Заявки за отпуски</h3>
+                <h3 className={styles["mrc-header__h3"]}>Потребителски портал</h3>
 
                 <div className={styles["mrc-header__header-notif-profile-div"]}>
                     <figure className={styles["header-notif-profile-div__figure"]}>
@@ -19,6 +43,11 @@ export default function RequestsClient () {
             </div>
 
             <div className={styles["main-requests-client__mrc-body"]}>
+                <div className={styles["mrc-body__user-greeting"]}>
+                    <h2 className={styles["user-greeting__h2"]}>Здравей, Иван!</h2>
+                    <p className={styles["user-greeting__p"]}>Добре дошъл в твоя потребителски портал.</p>
+                </div>
+
                 <div className={styles["mrc-body__days-off-new-request"]}>
                     <div className={styles["days-off-new-request__days-off"]}>
                         <p className={styles["days-off__num"]}>20</p>
@@ -27,6 +56,31 @@ export default function RequestsClient () {
 
                     <div className={styles["days-off-new-request__new-request"]}>
                         <button className={styles["new-request__button"]}>Нова заявка</button>
+                    </div>
+                </div>
+
+                <div className={styles["mrc-body__user-dashboard"]}>
+                    <h3 className={styles["user-dashboard__h3"]}>Таблото на Иван</h3>
+                    <div className={styles["user-dashboard__user-db-req"]}>
+                        <div className={styles["user-db-req__user-db-divs"]}>
+                            <h4 className={styles["user-db-divs__h4"]}>Текущи задачи</h4>
+                            
+                            {allTasks()}
+                            
+                            
+                        </div>
+
+                        <div className={styles["user-db-req__user-db-divs"]}>
+                            <h4 className={styles["user-db-divs__h4"]}>Отпуски</h4>
+                        </div>
+
+                        <div className={styles["user-db-req__user-db-divs"]}>
+                            <h4 className={styles["user-db-divs__h4"]}>Текущи задачи</h4>
+                        </div>
+
+                        <div className={styles["user-db-req__user-db-divs"]}>
+                            <h4 className={styles["user-db-divs__h4"]}>Текущи задачи</h4>
+                        </div>
                     </div>
                 </div>    
             </div>
